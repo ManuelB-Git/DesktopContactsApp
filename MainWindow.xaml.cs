0d16034cb4def2d3f36e35a8994917d6cfa7173e
Juan Manuel Benitez <jm_benitez@outlook.com>
2025-03-20 13:21:24 -0500

    Add NewContact button and window functionality
    Updated MainWindow.xaml to include a "NewContact" button
    that triggers the NewContactButton_Click event.
    Refactored MainWindow.xaml.cs to remove unused using
    directives and implement the event handler to open
    a new contact window. Cleaned up NewContactWindow.xaml.cs
    by removing unnecessary using directives and defining
    the NewContactWindow class.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,20 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace DesktopContactsApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void NewContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new();
+            newContactWindow.ShowDialog();
         }
     }
 }
--- NewContactWindow.xaml.cs
+++ NewContactWindow.xaml.cs
@@ -1,12 +1,9 @@
-﻿
-using System.Windows;
+﻿using System.Windows;
 
 
 namespace DesktopContactsApp
 {
-    /// <summary>
-    /// Interaction logic for NewContactWindow.xaml
-    /// </summary>
+
     public partial class NewContactWindow : Window
     {
         public NewContactWindow()
